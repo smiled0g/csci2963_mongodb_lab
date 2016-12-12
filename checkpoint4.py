fdfe466082392e312e7cc5fb06f6dd4c3969ca85
Kasi Chonpimai <mnm630848@gmail.com>
2016-12-12 09:39:18 -0500

    Add lab work

--- checkpoint4.py
+++ checkpoint4.py
@@ -2,4 +2,27 @@ from pymongo import MongoClient
 client = MongoClient()
 
 if __name__ == '__main__':
-    print "Modify me"
+
+	db = client.csci2963
+	collection = db.definitions
+
+	# find all
+	print "====== FIND ALL ======"
+	print [word_def for word_def in collection.find()]
+
+
+	print "====== FIND ONE ======"
+	print collection.find_one()
+
+
+	print "====== FIND SPECIFIC (smile) ======"
+	print collection.find_one({ "word": "smile" })
+
+
+	print "====== FIND BY ID (smile) ======"
+	print collection.find_one({ "_id" : "56fe9e22bad6b23cde07b8db" })
+
+
+	print "====== INSERT (dog) ======"
+	print collection.insert({ "word": "dog", "definition": " n. a domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, and a barking, howling, or whining voice. It is widely kept as a pet or for work or field sports." })
+
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,14 +1,33 @@
 from pymongo import MongoClient
+from random import randrange
+import datetime
 client = MongoClient()
 
 
-def random_word_requester():
+def random_word_requester(collection):
     '''
     This function should return a random word and its definition and also
     log in the MongoDB database the timestamp that it was accessed.
     '''
-    return
+
+    all_words = [word for word in collection.find()]
+    selected = all_words[randrange(len(all_words))]
+
+    collection.update(
+    	{ "_id": selected['_id'] },
+    	{
+    	  "$push": { "data": datetime.datetime.utcnow() }
+    	}
+    )
+
+    selected = collection.find_one({ "_id": selected['_id'] })
+
+    return selected
 
 
 if __name__ == '__main__':
-    print random_word_requester()
+
+	db = client.csci2963
+	collection = db.definitions
+
+	print random_word_requester(collection)
